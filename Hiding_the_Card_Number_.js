413c240fce287c7ca0ef528ece68591eb27577c5
NabilBennai <bennai.nabil@gmail.com>
2020-06-03 15:29:12 +0200

    Done by June 3rd 2020 @ 15:29 (UTC+2)

---
+++ Hiding_the_Card_Number_.js
@@ -0,0 +1,20 @@
+/*
+
+Hiding the Card Number
+
+Write a function that takes a credit card number and only displays the last four characters. The rest of the card number must be replaced by ************.
+
+Examples
+
+cardHide("1234123456785678") ➞ "************5678"
+cardHide("8754456321113213") ➞ "************3213"
+cardHide("35123413355523") ➞ "**********5523"
+
+Ensure you return a string.
+The length of the string must remain the same as the input.
+
+*/
+
+function cardHide(card) {
+  return "*".repeat(card.length - 4) + card.substring(card.length - 4);
+}
